79c94668e9b8822bcb94d16c9dc89eb1a5a96cb8
ASelim85 <asayed.cne@gmail.com>
2022-12-07 13:53:46 +0200

    initial commit

--- src/main/java/Pages/DashboardPage.java
+++ src/main/java/Pages/DashboardPage.java
@@ -27,7 +27,7 @@ public class DashboardPage extends PageBase{
     public String P2PCount;
 
     public By btnViewDetials = By.xpath("//span[contains(@class,'ant-typography _view-details-text_617v1_54')]");
-    public static By txtDashboardTitle = By.xpath("//*[@id=\"root\"]/div/section/section/main/div/h1");
+    public static By txtDashboardTitle = By.xpath("//*[@id=\"root\"]/div/section/section/main/div/div/div[1]/h1");
     public static By txtVolumeValue = By.xpath("//span[contains(@class,'volume-amount')]");
     public By txtVolumetarnscations = By.xpath("//span[contains(@class,'_volume-transaction')]");
     public By txtRevenueValue = By.xpath("//span[contains(@class,'revenue-amount')]");
